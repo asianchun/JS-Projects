c67765f08d43426127d9d6b74770ff3a0de65100
Hong Yu Yi <you@example.com>
2022-02-07 11:57:46 +1100

    tabs complete

---
+++ Tabs/index.js
@@ -0,0 +1,18 @@
+const tabs = document.querySelectorAll("[data-target]")
+const tabContents = document.querySelectorAll("[data-content]")
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    const target = document.querySelector(tab.dataset.target)
+
+    tabContents.forEach((tabContent) => {
+      tabContent.classList.remove("active")
+    })
+    target.classList.add("active")
+
+    tabs.forEach((tab) => {
+      tab.classList.remove("active")
+    })
+    tab.classList.add("active")
+  })
+})
